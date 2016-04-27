35ed8c5de66466fb82133fd0fc494509d6514c82
jruel006 <jruel006@ucr.edu>
2016-04-26 20:45:33 -0700

    adding tuber.tech to auth for fb

--- config/fb.js
+++ config/fb.js
@@ -1,7 +1,7 @@
 
 var facebook_client_id = '1677423189189016';
 var facebook_client_secret = '9e9e60395b6d7ac9ad5e7847faa214bd';
-var facebook_callback_url = 'http://localhost:3000/auth/facebook/callback';
+var facebook_callback_url = 'http://tuber.tech/auth/facebook/callback';
 
 module.exports = {
     'clientID'      : facebook_client_id , // your App ID
