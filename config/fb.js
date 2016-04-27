e17c8005bb463853c7207b10b04c81a629130158
jruel006 <jruel006@ucr.edu>
2016-04-26 21:32:02 -0700

    Starting to get my shit together

--- config/fb.js
+++ config/fb.js
@@ -3,9 +3,12 @@ var facebook_client_id = '1677423189189016';
 var facebook_client_secret = '9e9e60395b6d7ac9ad5e7847faa214bd';
 var facebook_callback_url = 'http://tuber.tech/auth/facebook/callback';
 
+var fb_id = "1677501949181140";
+var fb_secret = "539d8fb1d4c9462a19a02c3988ef98fc";
+var fb_callback = 'http://localhost:3000/auth/facebook/callback'
+
 module.exports = {
-    'clientID'      : facebook_client_id , // your App ID
-    'clientSecret'  : facebook_client_secret, // your App Secret
-    'callbackURL'   : facebook_callback_url,
-    'redirect_uri'  : facebook_callback_url
+    'clientID'      : fb_id || facebook_client_id , // your App ID
+    'clientSecret'  : fb_secret || facebook_client_secret, // your App Secret
+    'callbackURL'   : fb_callback || facebook_callback_url
 }
