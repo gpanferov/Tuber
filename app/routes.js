ca4ac4e9366f7db8a829810e9a64049eabaf2915
jruel006 <jruel006@ucr.edu>
2016-04-28 02:20:04 -0700

    Trying to get the profile pic to show up the right way

--- app/routes.js
+++ app/routes.js
@@ -15,9 +15,12 @@ module.exports = function(app, passport) {
                         tutors: null
                     })
                 } else {
+
+                    var tutor_array = chunkify(users, Math.round(Math.sqrt(users.length)), true );
+                    console.log(tutor_array)
                     res.render('tuber.ejs', {
                         user: req.user,
-                        tutors: users
+                        tutors: tutor_array
                     })
                 }
             })
@@ -60,34 +63,44 @@ module.exports = function(app, passport) {
         failureRedirect: '/'
     }),
     function(err,req,res,next) {
-        // You could put your own behavior in here, fx: you could force auth again...
-        // res.redirect('/auth/facebook/');
-        res.redirect('/');
-        if(err) {
-            res.status(400);
-            res.render('error', {message: err.message});
-        }
-    }
-
+          // You could put your own behavior in here, fx: you could force auth again...
+          // res.redirect('/auth/facebook/');
+          if(err) {
+              console.log(err)
+          }
+          res.redirect('/');
+      }
     );
-    /*************PROTECTED ROUTES *****************/
+    /***************** PROTECTED ROUTES *****************/
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user: req.user // get the user out of session and pass to template
         });
     });
 
-
-
-
+    app.get('/chat/:id', function(req, res){
+      // TODO create a chatroom
+    })
     app.get('/users', isLoggedIn, function(req, res) {
-        // return users json
+        // TODO return users json
     });
     app.get('/user/:id', isLoggedIn, function(req, res) {
-        // return one user
+        // TODO return one user
     });
-    app.put('/users/:id', isLoggedIn, function(req, res) {});
-    app.get('/classes', isLoggedIn, function(req, res) {});
+    app.put('/users/:id', isLoggedIn, function(req, res) {
+      // TODO return one user
+    });
+    app.get('/classes', isLoggedIn, function(req, res) {
+      // TODO return all classes to be browsed
+    });
+    app.get('/classes/:id', isLoggedIn, function(req, res){
+      // TODO return all tutors for that class
+    });
+
+    app.get('/tutors/:id', isLoggedIn, function(req, res){
+      // TODO return a tutor's profile
+    })
+
     /***This is used for our api for our mobile devices****/
 };
 // route middleware to make sure a user is logged in
@@ -103,4 +116,46 @@ function isAuthenticated(req) {
         return true
     }
     return false
+}
+
+
+
+function chunkify(a, n, balanced) {
+
+    if (n < 2)
+        return [a];
+
+    var len = a.length,
+            out = [],
+            i = 0,
+            size;
+
+    if (len % n === 0) {
+        size = Math.floor(len / n);
+        while (i < len) {
+            out.push(a.slice(i, i += size));
+        }
+    }
+
+    else if (balanced) {
+        while (i < len) {
+            size = Math.ceil((len - i) / n--);
+            out.push(a.slice(i, i += size));
+        }
+    }
+
+    else {
+
+        n--;
+        size = Math.floor(len / n);
+        if (len % size === 0)
+            size--;
+        while (i < size * n) {
+            out.push(a.slice(i, i += size));
+        }
+        out.push(a.slice(size * n));
+
+    }
+
+    return out;
 }
