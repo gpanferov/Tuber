f9e40fd7b3687e29490871795c474bb1b586f4e5
jruel006 <jruel006@ucr.edu>
2016-04-27 22:36:49 -0700

    got rid of the 'return done' part of authentication

--- config/passport.js
+++ config/passport.js
@@ -23,31 +23,36 @@ module.exports = function(passport) {
     });
     passport.use('local-signup', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
-        usernameField: 'email',
+        usernameField: 'username',
         passwordField: 'password',
         passReqToCallback: true // allows us to pass back the entire request to the callback
-    }, function(req, email, password, done) {
+    }, function(req, username, password, done) {
+
+        console.log(req.body)
         // asynchronous
         // User.findOne wont fire unless data is sent back
         process.nextTick(function() {
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
             User.findOne({
-                'local.email': email
+                'local.username': username
             }, function(err, user) {
                 // if there are any errors, return the error
                 if (err) return done(err);
                 // check to see if theres already a user with that email
                 if (user) {
-                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                    return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
                 } else {
                     // if there is no user with that email
                     // create the user
                     var newUser = new User();
                     // set the user's local credentials
-                    newUser.local.email = email;
-                    newUser.local.password = newUser.generateHash(password);
+                    newUser.local.email = req.body.email;
+                    newUser.local.password = newUser.generateHash(req.body.password);
                     newUser.local.username = req.body.username
+                    newUser.local.first = req.body.first;
+                    newUser.local.last = req.body.last;
+
                         // save the user
                     newUser.save(function(err, user) {
                         if (err) {
@@ -60,7 +65,6 @@ module.exports = function(passport) {
                                 if (err) {
                                     console.log(err)
                                 } else {
-                                    return
                                     done(null, newUser);
                                 }
                             })
@@ -75,22 +79,30 @@ module.exports = function(passport) {
         usernameField: 'username',
         passwordField: 'password',
         passReqToCallback: true // allows us to pass back the entire request to the callback
-    }, function(req, email, password, done) { // callback with email and password from our form
+    }, function(req, username, password, done) { // callback with email and password from our form
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
         console.log("WE IN NIGGA")
         User.findOne({
-            'local.username': email
+            'local.username': username
         }, function(err, user) {
             // if there are any errors, return the error before anything else
-            if (err) return done(err);
+            if (err){
+              console.log(err)
+              done(err);
+            }
             // if no user is found, return the message
-            if (!user) return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            if (!user){
+              console.log("No user with those credentials")
+              done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            }
             // if the user is found but the password is wrong
-            if (!user.validPassword(password)) return
-            done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+            if (!user.validPassword(password)){
+              console.log("Wrong password")
+              done(null, false, req.flash('loginMessage', 'Oops! Wrong Username/Password.')); // create the loginMessage and save it to session as flashdata
+            }
             // all is well, return successful user
-            return done(null, user);
+            done(null, user);
         });
     }));
 
